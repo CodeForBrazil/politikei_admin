73212f458a3e7dd2aed86090c08030f37814d5ba
Murilo Lima <murilo.wlima@gmail.com>
2015-11-08 21:17:04 -0200

    Implementado funcionalidade de reservar/liberar proposicao pelos colaboradores

--- web/application/models/user_model.php
+++ web/application/models/user_model.php
@@ -188,6 +188,7 @@ class User_model extends MY_Model
     public function get_by_email($email)
     {
         $query = $this->db->get_where(self::TABLE_NAME, array('email' => $email));
+        print_r($query);
         return $this->get_first_self_result($query);
     }
 
