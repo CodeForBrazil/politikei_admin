c14fa708eafcf7179fc4588b8857dbf30709d735
unknown <murilo@MURILO.mps.interno>
2015-11-20 15:37:50 -0200

    Ajuste scrip de migracao do banco

--- db/scripts/2015-11-04_TD_1_Merge_CodeIgniter_Tables.sql
+++ db/scripts/2015-11-04_TD_1_Merge_CodeIgniter_Tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `media` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `public_id` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varbinary(255) DEFAULT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `avatar`, `bio`, `city`, `alias`, `roles`, `confirmation`, `dateadd`, `dateupdate`, `status`)
 VALUES
-	(1, 'Admin', 'admin@sand.box', X'1464A86EAB99', NULL, '', '', '', 1, NULL, '2015-09-17 12:22:12', '2015-09-17 14:06:57', 1);
+  (1, 'Admin', 'admin@sand.box', X'1464A86EAB99', NULL, '', '', '', 1, NULL, '2015-09-17 12:22:12', '2015-09-17 14:06:57', 1);
 
 ALTER TABLE `votos` CHANGE `usuario_id` `user_id` INT(11)  NOT NULL;
 
@@ -34,4 +34,12 @@ ALTER TABLE `palamentares_favoritos` CHANGE `usuario_id` `user_id` INT(11)  NOT 
 
 ALTER TABLE `palamentares_favoritos` CHANGE `id_parlamentar` `parlamentar_id` INT(11)  NOT NULL;
 
+ALTER TABLE `palamentares_favoritos` DROP FOREIGN KEY  `fk_Usuario_has_Parlamentares_Usuario1`;
+ALTER TABLE `votos` DROP FOREIGN KEY  `fk_Votos_Usuario1`;
+
 DROP TABLE `usuarios`;
+
+ALTER TABLE `politikei`.`palamentares_favoritos` ADD CONSTRAINT `fk_Usuario_has_Parlamentares_Usuario`  FOREIGN KEY (`user_id`)  REFERENCES `politikei`.`user` (`id`);
+
+ALTER TABLE `politikei`.`votos` ADD CONSTRAINT `fk_Votos_Usuario`  FOREIGN KEY (`user_id`)  REFERENCES `politikei`.`user` (`id`);
+
